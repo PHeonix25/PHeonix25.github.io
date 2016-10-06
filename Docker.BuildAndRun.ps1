c81fa91c6aae830b8011568814b30e73c630d481
PHeonix25 <p@hermens.com.au>
2016-10-06 23:46:05 +0200

    Updating docker files to reduce build times

--- Docker.BuildAndRun.ps1
+++ Docker.BuildAndRun.ps1
@@ -1,2 +1,8 @@
+# Build the image (called blog, version 'latest')
 docker build . -t blog:latest 
+
+# Run an instance of the image in the background, opening port 4000 as well
 docker run -d -p 4000:4000 -t blog:latest
+
+# Launch the browser so that we can check our work
+start 'http://0.0.0.0:4000/'
